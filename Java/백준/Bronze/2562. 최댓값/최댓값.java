ba10078dfd132d7ca7c2903acb85c97eb75966c9
jemin cha <113026233+jemincha@users.noreply.github.com>
2026-02-11 15:33:25 +0900

    [Bronze III] Title: 최댓값, Time: 168 ms, Memory: 17684 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int max = 0;
+        int index = 0;
+
+        for (int i = 1; i <= 9; i++) {
+            int current = sc.nextInt();
+            
+            if (current > max) {
+                max = current;
+                index = i;
+            }
+        }
+        System.out.println(max);
+        System.out.println(index);
+               
+        sc.close();
+    }
+}
