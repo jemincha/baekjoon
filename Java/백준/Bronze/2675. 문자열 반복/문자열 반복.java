cd92063e377b4ed5cde7bef316b90f0dc55c4548
jemin cha <113026233+jemincha@users.noreply.github.com>
2026-02-19 17:50:26 +0900

    [Bronze II] Title: 문자열 반복, Time: 104 ms, Memory: 13992 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int R = Integer.parseInt(st.nextToken());
+            String S = st.nextToken();
+
+            for (int i = 0; i < S.length(); i++) {
+                for (int j = 0; j < R; j++) {
+                    sb.append(S.charAt(i));
+                }
+            }
+
+            sb.append("\n");
+        }
+
+        System.out.print(sb.toString());
+        br.close();
+    }
+}
