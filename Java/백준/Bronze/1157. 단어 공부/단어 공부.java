09852227371468d54182a46351bf7a42a5a85b4b
jemin cha <113026233+jemincha@users.noreply.github.com>
2026-02-25 18:11:00 +0900

    [Bronze I] Title: 단어 공부, Time: 252 ms, Memory: 21136 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String s = br.readLine().toUpperCase();
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'A']++;  
+        }
+
+        int max = -1;
+        char result = '?';
+
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > max) {
+                max = count[i];
+                result = (char) (i + 'A'); 
+            } 
+            else if (count[i] == max) {
+                result = '?';
+            }
+        }
+
+        System.out.println(result);
+        br.close();
+    }
+}
